aaf9ab1b43e94bb59436b4bd51527ec8c1040442
rikardonm <rikardo.nm@hotmail.com>
2014-03-28 16:15:41 -0300

    macros for project name and splash screen enables
    1-> enables
    0-> disables

--- depl_spc/globalParam.h
+++ depl_spc/globalParam.h
@@ -10,6 +10,11 @@
 
 
 
+#define PROJECT_NAME        ("your projects name here")
+#define LCD_SPLASHSCREEN1   1   //enables proejct name in 2 secs splash
+#define LCD_SPLASHSCREEN    1   //enables date and time of compilation
+
+
 #define CPU_CLOCK	48
 #define BUS_CLOCK	CPU_CLOCK/2
 #define CPUHZ_CLOCK	48000000
